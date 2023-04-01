fa84315212572dda12a63661710e024c6fd1396d
James Sanchez <mr_jimmybob@yahoo.com>
2023-04-01 12:59:59 +0100

    Refactor PDF creation
    Refactoring of PDF document creation.

--- back/api/modules/utils.js
+++ back/api/modules/utils.js
@@ -1,10 +1,12 @@
 const path = require('path')
 const fs = require('fs')
 
+const convertHTMLToPDF = require('pdf-puppeteer')
+const pdfFormat = require('../config/pdf')
+
 function fullFileName(template) {
     return path.resolve('./public', 'templates') + '/' + template
 }
-
 
 function writePdfFile(prefix, pdf)
 {
@@ -13,7 +15,30 @@ function writePdfFile(prefix, pdf)
       console.log('%c Error saving CARE contract: %o', 'color: red', err)
     })
 }
-          
 
+function renderPdf(res, pdfParams, htmlTemplateFile, filePrefix)
+{
+  return res.render(
+    htmlTemplateFile, pdfParams,
+    (err, html) => {
+      convertHTMLToPDF(
+        html,
+        (pdf) => {
+          writePdfFile(filePrefix, pdf)
+          res.setHeader('Content-Type', 'application/pdf')
+          res.send(pdf)
+        },
+        pdfFormat,
+        null,
+        true
+      ).catch((err) => {
+        console.log(err)
+        res.status(500).send(err)
+      })
+    }
+  )
+}
+
+module.exports['renderPdf'] = renderPdf;
 module.exports['fullFileName'] = fullFileName;
 module.exports['writePdfFile'] = writePdfFile;
--- back/api/routes/care4.js
+++ back/api/routes/care4.js
@@ -1,40 +1,19 @@
 const express = require('express')
 const router = express.Router()
 
-const convertHTMLToPDF = require('pdf-puppeteer')
-const datos = require('../config/care')
-const pdfFormat = require('../config/pdf')
-const {fullFileName, writePdfFile} = require('../modules/utils')
+const { fullFileName, renderPdf } = require('../modules/utils')
+
+const data = require('../config/care')
 
 router.get('/', (req, res, next) => {
-  const templateFile = fullFileName('CARE4.ejs')
-  const prefix = 'CARE4_'
 
-  const params = {
-    datos: datos,
-    datosModelo: datos.datosModelo,
+  const pdfParams = {
+    datos: data,
+    datosModelo: data.datosModelo,
     resUrl: 'http://localhost:9000/'
   };
 
-  return res.render(
-    templateFile, params,
-    (err, html) => {
-      convertHTMLToPDF(
-        html,
-        (pdf) => {
-          writePdfFile(prefix, pdf)
-          res.setHeader('Content-Type', 'application/pdf')
-          res.send(pdf)
-        },
-        pdfFormat,
-        null,
-        true
-      ).catch((err) => {
-        console.log(err)
-        res.status(500).send(err)
-      })
-    }
-  )
+  renderPdf(res, pdfParams, fullFileName('CARE4.ejs'), 'CARE4_')
 })
 
 module.exports = router
